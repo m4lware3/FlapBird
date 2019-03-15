e0c231429e77aa755d7d9a2b37f0cad18fa190cb
loki <0x1dfc@protonmail.com>
2019-03-15 08:15:15 -0300

    Add imagens

--- core/src/com/cursoandroid/flapbird/FlapBird.java
+++ core/src/com/cursoandroid/flapbird/FlapBird.java
@@ -7,16 +7,35 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlapBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private SpriteBatch batch;
+	private Texture passaro;
+    private Texture fundo;
+    private int largura;
+    private int altura;
+	private int movimento =0;
+
 	@Override
 	public void create () {
 
+	    batch = new SpriteBatch();
+	    passaro = new Texture("passaro1.png");
+	    fundo = new Texture("fundo.png");
+
+	    largura = Gdx.graphics.getWidth();
+	    altura = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
+
+	    movimento++;
+	    batch.begin();
+
+	    batch.draw(fundo,0,0,largura,altura);
+	    batch.draw(passaro,movimento,0,280,188);
+
+	    batch.end();
 
 	}
 	
