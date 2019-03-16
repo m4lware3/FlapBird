b2dd9ad1a124e2406c17d1e4277f0fe7bf722eaa
loki <0x1dfc@protonmail.com>
2019-03-16 08:16:32 -0300

    Muitas mudanças rsrsrs

--- core/src/com/cursoandroid/flapbird/FlapBird.java
+++ core/src/com/cursoandroid/flapbird/FlapBird.java
@@ -6,67 +6,83 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class FlapBird extends ApplicationAdapter {
 
-	private SpriteBatch batch;
-	private Texture[] passaro;
+    private SpriteBatch batch;
+    private Texture[] passaros;
     private Texture fundo;
-    private int largura;
-    private int altura;
-	private float variacao =0;
-	private float velocidadeQueda =0;
-	private float posicaoInicialVertical =0;
+    private Texture canoBaixo;
+    private Texture canoTopo;
+    private Random numeroRandomico;
 
-	@Override
-	public void create () {
+    //Atributos de configuracao
+    private int larguraDispositivo;
+    private int alturaDispositivo;
 
-	    batch = new SpriteBatch();
-	    passaro = new Texture[3];
-	    passaro[0] = new Texture("passaro1.png");
-        passaro[1] = new Texture("passaro2.png");
-        passaro[2] = new Texture("passaro3.png");
+    private float variacao = 0;
+    private float velocidadeQueda=0;
+    private float posicaoInicialVertical;
+    private float posicaoMovimentoCanoHorizontal;
+    private float espacoEntreCanos;
+    private float deltaTime;
+    private float alturaEntreCanosRandomica;
+
+    @Override
+    public void create () {
+
+        batch = new SpriteBatch();
+        numeroRandomico = new Random();
+        passaros = new Texture[3];
+        passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
 
         fundo = new Texture("fundo.png");
+        canoBaixo = new Texture("cano_baixo_maior.png");
+        canoTopo = new Texture("cano_topo_maior.png");
 
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo  = Gdx.graphics.getHeight();
+        posicaoInicialVertical = alturaDispositivo / 2;
+        posicaoMovimentoCanoHorizontal = larguraDispositivo;
+        espacoEntreCanos = 300;
 
-	    largura = Gdx.graphics.getWidth();
-	    altura = Gdx.graphics.getHeight();
-        posicaoInicialVertical = altura/2;
-	}
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render () {
 
-	    //Velocidade da animação
-	    variacao += Gdx.graphics.getDeltaTime() * 5;
+        deltaTime = Gdx.graphics.getDeltaTime();
 
+        variacao += deltaTime * 10 ;
+        posicaoMovimentoCanoHorizontal -= deltaTime * 200;
         velocidadeQueda++;
 
-        //Não deixa a variação de img passar de 2 (0,1,2)
-	    if (variacao > 2) variacao = 0;
+        if(variacao > 2) variacao = 0;
 
-        //Evento touch
-        if(Gdx.input.justTouched())
-            velocidadeQueda = -20;
+        if( Gdx.input.justTouched() ){
+            velocidadeQueda = -15;
+        }
 
+        if(posicaoInicialVertical > 0 || velocidadeQueda < 0 )
+            posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
 
-	    //Velocidade de queda
+        //Verifica se o cano saiu inteiramente da tela
+        if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth() ){
+            posicaoMovimentoCanoHorizontal = larguraDispositivo;
+            alturaEntreCanosRandomica = numeroRandomico.nextInt(1600) - 800;
+        }
 
-        if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
-            posicaoInicialVertical += -velocidadeQueda;
+        batch.begin();
 
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw( canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica );
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2- canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica );
+        batch.draw( passaros[ (int) variacao ] , 120, posicaoInicialVertical );
 
-	    batch.begin();
+        batch.end();
 
-	    batch.draw(fundo,0,0,largura,altura);
-	    batch.draw(passaro[(int)variacao],30,posicaoInicialVertical,280,188);
-
-	    batch.end();
-
-	}
-	
-	@Override
-	public void dispose () {
-
-	}
+    }
 }
